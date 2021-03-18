d361057b91f6eccc0aaa0a1274e8adca771a54bb
AkashDeepSinghJassal <akashdeepjassal@gmail.com>
2021-03-18 16:02:58 +0530

    use prop-types

--- src/App.js
+++ src/App.js
@@ -1,8 +1,5 @@
-import logo from './logo.svg';
 import './App.css';
 import React from 'react';
-import Input from './input'
-import axios from 'axios';
 import NavbarHead from './components/navbar/navbarHead';
 import Details from'./components/Details/details'
 
